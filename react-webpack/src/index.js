9ff591f6d477d9d96bdb2a37a41b8571df89c90e
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2023-12-08 06:36:05 +0900

    update react to 18 version

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // create a new component. This component should produce some HTML
 
@@ -13,4 +13,6 @@ function App() {
 }
 
 //render this component to the screen
-ReactDOM.render(<App />, document.querySelector("#root"));
+// ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
