4be5916f8a31f059084d1003b6ad47fe8c33daea
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-12-13 10:53:19 +0900

    DOM event with useEffect Learning how to use useEffect when have dependencies Learning how to use useEffect width event resize, scroll Learning about add, remove DOM event Avoid memory leak with removeEventListener when component deleting

--- tiktok-clone/src/UseEffect1.js
+++ tiktok-clone/src/UseEffect1.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 
+const tabs = ["posts", "comments", "albums", "photos", "todos", "users"]
+
 const UseEffect1 = () => {
 	//1. useEffect(callback)
 	//- callback: run after component render
@@ -10,24 +12,76 @@ const UseEffect1 = () => {
 
 	//-------------------
 	// Callback always run when component render
+	// cleanup function run before component render
 
 	const [title, setTitle] = useState("")
-	const [posts, setPosts] = useState([])
-
 	useEffect(() => {
 		document.title = title
 	})
+
+	const [posts, setPosts] = useState([])
+	const [type, setType] = useState("posts")
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/posts")
+		fetch(`https://jsonplaceholder.typicode.com/${type}`)
 			.then((res) => res.json())
 			.then((posts) => {
 				setPosts(posts)
 			})
+	}, [type])
+
+	const [showGoTop, setShowGoTop] = useState(false)
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY >= 200) {
+				setShowGoTop(true)
+			} else {
+				setShowGoTop(false)
+			}
+		}
+		window.addEventListener("scroll", handleScroll)
+		return () => {
+			window.removeEventListener("scroll", handleScroll)
+		}
 	}, [])
 
+	const [size, setSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	})
+
+	const handleResize = () => {
+		setSize({
+			width: window.innerWidth,
+			height: window.innerHeight,
+		})
+	}
+	useEffect(() => {
+		window.addEventListener("resize", handleResize)
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
 	return (
 		<div>
 			<h1>UseEffect1</h1>
+			<h2>Width: {size.width}</h2>
+			<h2>height: {size.height}</h2>
+			{tabs.map((tab, index) => (
+				<button
+					key={index}
+					onClick={() => setType(tab)}
+					style={
+						type === tab
+							? {
+									color: "#fff",
+									backgroundColor: "#333",
+							  }
+							: {}
+					}
+				>
+					{tab}
+				</button>
+			))}
 			<input
 				type="text"
 				value={title}
@@ -35,9 +89,22 @@ const UseEffect1 = () => {
 			/>
 			<ul>
 				{posts.map((post) => (
-					<li key={post.id}>{post.title}</li>
+					<li key={post.id}>{post.title || post.name}</li>
 				))}
 			</ul>
+			{showGoTop && (
+				<button
+					onClick={() => {
+						window.scrollTo({
+							top: 0,
+							behavior: "smooth",
+						})
+					}}
+					style={{ position: "fixed", bottom: 20, right: 20 }}
+				>
+					Go top
+				</button>
+			)}
 		</div>
 	)
 }
